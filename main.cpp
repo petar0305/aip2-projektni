34aff1dc3e9cc832a6b68628bedccf1b880df7bc
petar0305 <petar14555@gmail.com>
2025-05-22 06:43:27 +0000

    Co-authored-by: Jurica Francetic <Franceticc@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <iostream>
+#include <iostream>
+#include <cstring>
+
 using namespace std;
 int main()
 {
-    cout << "hello world";
     cout << "hello world";
     cout << "hello world";
 }
