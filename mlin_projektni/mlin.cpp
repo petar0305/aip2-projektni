2b7101eeee1192dfd4a7a02170bce7a1bca4f62d
petar0305 <petar14555@gmail.com>
2025-05-28 20:44:35 +0000

    pocel sam nekaj za izbor 2

--- mlin_projektni/mlin.cpp
+++ mlin_projektni/mlin.cpp
@@ -3,24 +3,26 @@
 #include <chrono>
 #include <thread>
 using namespace std;
-void clearConsole()
+void clearScreen()
 {
-  cout << "\033[2J\033[1;1H";
+  cout << string(100, '\n');
 }
 void ispisPloce()
 {
   for (int i = 0; i < 9; i++)
   {
     for (int j = 0; j < 17; j++)
-      cout << "j";
+      cout << "j"; 
   }
 }
 
 int slova_u_koordinate(char figura)
 {
-  if (figura ==)
-}
+  if (figura =='A'){
 
+  }
+}
+/*
 int figure_igrac1()
 {
   if (ploca[0][0] != emoji2 && ploca[0][0] != emoji)
@@ -51,6 +53,9 @@ int figure_igrac2()
       igrac1_uzima(ploca, emoji2);
   }
 }
+*/
+
+
 
 int main()
 {
@@ -58,12 +63,13 @@ int main()
 
   while (1)
   {
-    cout << "1. Pravila igre" << endl;
-    cout << "2. Igraj" << endl;
-    cout << "3. Ispis leaderboarda" << endl;
-    cout << "4. Izlaz" << endl;
-
+    cout << "\t\t1. Pravila igre" << endl;
+    cout << "\t\t2. Igraj" << endl;
+    cout << "\t\t3. Ispis leaderboarda" << endl;
+    cout << "\t\t4. Izlaz" << endl;
+    cout << "\t\tUnesite vaš izbor: ";
     cin >> izbor;
+    clearScreen();
 
     if (izbor == 1)
     {
@@ -72,10 +78,32 @@ int main()
 
     else if(izbor==2)
     {
-      
+      string igrac1, igrac2;
+      cout << "Unesite ime 1. igrača: ";
+      cin >> igrac1;
+      cout << "Unesite ime 2. igrača: ";
+      cin >> igrac2;
+      clearScreen();
+      string ploca = {'A','-','-','-','-','-','-','-','B','-','-','-','-','-','-','-','C'};
+      /*A---------B---------C
+      | D-------E-------F |
+      | | G-----H-----I | |
+      | | |           | | |
+      J-K-L           M-N-O
+      | | |           | | |
+      | | P-----Q-----R | |
+      | S-------T-------U |
+      V---------W---------X*/
+
+      cout << ploca;
     }
+    else if(izbor==3){
+
+    }
+    else if(izbor==4)
+      break;
   }
-  for (int i = 0; i < 18; i++)
+  /*for (int i = 0; i < 18; i++)
   {
     if (!i % 2)
     {
@@ -85,5 +113,5 @@ int main()
     {
       cout << "igrac 1 je na redu.";
     }
-  }
+  }*/
 }
