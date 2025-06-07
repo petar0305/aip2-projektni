10e349127c3194f6abadac7a01863ab5c205832c
petar0305 <petar14555@gmail.com>
2025-06-07 21:16:14 +0000

    treba napraviti još samo za spremanje u txt i binarnu to bum sutra sam treba skuzit kak da sve spremim i od kud se treba nastavit

--- mlin_projektni/mlin.cpp
+++ mlin_projektni/mlin.cpp
@@ -6,6 +6,31 @@ using namespace std;
 const string emoji = "●";
 const string emoji2 = "○";
 int brojac1 = 0, brojac2 = 0;
+const string susjedi[24][5] = {
+    {"A", "B", "J", "", ""},
+    {"B", "A", "C", "E", ""},
+    {"C", "B", "O", "", ""},
+    {"D", "E", "K", "", ""},
+    {"E", "B", "D", "F", "H"},
+    {"F", "E", "N", "", ""},
+    {"G", "H", "L", "", ""},
+    {"H", "E", "G", "I", ""},
+    {"I", "H", "M", "", ""},
+    {"J", "A", "K", "V", ""},
+    {"K", "D", "J", "L", "S"},
+    {"L", "G", "K", "P", ""},
+    {"M", "I", "N", "R", ""},
+    {"N", "F", "M", "O", "U"},
+    {"O", "C", "N", "X", ""},
+    {"P", "L", "Q", "", ""},
+    {"Q", "P", "R", "T", ""},
+    {"R", "M", "Q", "", ""},
+    {"S", "K", "T", "", ""},
+    {"T", "Q", "S", "U", "W"},
+    {"U", "N", "T", "", ""},
+    {"V", "J", "W", "", ""},
+    {"W", "T", "V", "X", ""},
+    {"X", "O", "W", "", ""}};
 struct Mlin
 {
   const char *a, *b, *c;
@@ -14,15 +39,23 @@ const Mlin sviMlinovi[16] = {
     {"A", "B", "C"}, {"D", "E", "F"}, {"G", "H", "I"}, 
     {"J", "K", "L"}, {"M", "N", "O"}, {"P", "Q", "R"}, 
     {"S", "T", "U"}, {"V", "W", "X"}, {"A", "J", "V"}, 
-    {"D", "K", "S"}, {"G", "H", "I"}, {"B", "E", "H"}, {"C", "F", "I"}, 
-    {"L", "M", "N"}, {"O", "R", "U"}, {"X", "W", "V"}};
+    {"D", "K", "S"}, {"G", "H", "I"}, {"B", "E", "H"}, 
+    {"C", "F", "I"}, {"L", "M", "N"},
+    {"O", "R", "U"}, {"X", "W", "V"}};
 struct Koordinata
 {
   const char *oznaka;
   int i, j;
 };
 const Koordinata pozicije[24] = {
-    {"A", 0, 0}, {"B", 0, 10}, {"C", 0, 20}, {"D", 1, 2}, {"E", 1, 10}, {"F", 1, 18}, {"G", 2, 4}, {"H", 2, 10}, {"I", 2, 16}, {"J", 4, 0}, {"K", 4, 2}, {"L", 4, 4}, {"M", 4, 16}, {"N", 4, 18}, {"O", 4, 20}, {"P", 6, 4}, {"Q", 6, 10}, {"R", 6, 16}, {"S", 7, 2}, {"T", 7, 10}, {"U", 7, 18}, {"V", 8, 0}, {"W", 8, 10}, {"X", 8, 20}};
+    {"A", 0, 0}, {"B", 0, 10}, {"C", 0, 20}, 
+    {"D", 1, 2}, {"E", 1, 10}, {"F", 1, 18}, 
+    {"G", 2, 4}, {"H", 2, 10}, {"I", 2, 16}, 
+    {"J", 4, 0}, {"K", 4, 2}, {"L", 4, 4}, 
+    {"M", 4, 16}, {"N", 4, 18}, {"O", 4, 20}, 
+    {"P", 6, 4}, {"Q", 6, 10}, {"R", 6, 16}, 
+    {"S", 7, 2}, {"T", 7, 10}, {"U", 7, 18}, 
+    {"V", 8, 0}, {"W", 8, 10}, {"X", 8, 20}};
 bool provjera_mlina(const string &polje, const string ploca[9][21], const string &figura)
 {
   for (int i = 0; i < 16; i++)
@@ -103,10 +136,98 @@ bool slova_u_koordinate(string polje, string ploca[9][21], int &x, int &y)
   }
   return false;
 }
-
+bool figura_je_u_mlinu(int &x, int &y, string ploca[9][21])
+{
+  string f = ploca[x][y];
+  if (f != "○" && f != "●")
+    return false;
+  string oznaka = "";
+  for (int i = 0; i < 24; i++)
+  {
+    if (pozicije[i].i == x && pozicije[i].j == y)
+    {
+      oznaka = pozicije[i].oznaka;
+      break;
+    }
+  }
+  if (oznaka == "")
+    return false;
+  for (int i = 0; i < 16; i++)
+  {
+    Mlin m = sviMlinovi[i];
+    if (oznaka == m.a || oznaka == m.b || oznaka == m.c)
+    {
+      Koordinata ka, kb, kc;
+      bool foundA = false, foundB = false, foundC = false;
+      for (int j = 0; j < 24; j++)
+      {
+        if (pozicije[j].oznaka == m.a)
+        {
+          ka = pozicije[j];
+          foundA = true;
+        }
+        if (pozicije[j].oznaka == m.b)
+        {
+          kb = pozicije[j];
+          foundB = true;
+        }
+        if (pozicije[j].oznaka == m.c)
+        {
+          kc = pozicije[j];
+          foundC = true;
+        }
+      }
+      if (foundA && foundB && foundC)
+      {
+        if (ploca[ka.i][ka.j] == f &&
+            ploca[kb.i][kb.j] == f &&
+            ploca[kc.i][kc.j] == f)
+        {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+int n_figura_izvan_mlina(string ploca[9][21], string protivnik)
+{
+  int n = 0;
+  for (int i = 0; i < 24; i++)
+  {
+    int x = pozicije[i].i;
+    int y = pozicije[i].j;
+    if (ploca[x][y] == protivnik && !figura_je_u_mlinu(x, y, ploca))
+      n++;
+  }
+  return n;
+}
 void igrac1_uzima(string ploca[9][21], string pocetna[9][21], int &x, int &y)
 {
-  string unos;//figura koju igrac jede(uzima)
+  if (n_figura_izvan_mlina(ploca, emoji2) == 0)
+  {
+    cout << "Sve protivničke figure su u mlinu. Možeš uzeti bilo koju." << endl;
+    string unos;
+    ispisPloce(ploca);
+    while (1)
+    {
+      cout << "Igrač 1, unesi polje figure koju želiš uzeti: ";
+      cin >> unos;
+      transform(unos.begin(), unos.end(), unos.begin(), ::toupper);
+      slova_u_koordinate(unos, pocetna, x, y);
+      if (ploca[x][y] == "○")
+      {
+        ploca[x][y] = pocetna[x][y];
+        clearScreen();
+        brojac2--;
+        break;
+      }
+      else
+        cout << "Na tom polju nije protivnička figura. Pokušaj ponovno." << endl;
+    }
+    return;
+  }
+  string unos; // figura koju igrac jede(uzima)
   ispisPloce(ploca);
   while(1){
     cout << "Igrač 1, unesi polje figure koju želiš uzeti: ";
@@ -114,17 +235,43 @@ void igrac1_uzima(string ploca[9][21], string pocetna[9][21], int &x, int &y)
     transform(unos.begin(), unos.end(), unos.begin(), ::toupper);
     slova_u_koordinate(unos, pocetna, x, y);
     if (ploca[x][y] == "○"){
+      if(figura_je_u_mlinu(x, y, ploca)){
+        cout << "Ta figura je u mlinu. Pokušaj ponovno" << endl;
+        continue;
+      }
       ploca[x][y] = pocetna[x][y];
       clearScreen();
-      brojac1--;
+      brojac2--;
       break;
     }
-    else{
+    else
       cout << "Na tom polju nije protivnička figura. Pokušaj ponovno." << endl;
-    }
   }
 }
 void igrac2_uzima(string ploca[9][21], string pocetna[9][21], int &x, int &y){
+  if (n_figura_izvan_mlina(ploca, emoji) == 0)
+  {
+    cout << "Sve protivničke figure su u mlinovima. Ne možeš ništa uzeti.\n";
+    string unos;
+    ispisPloce(ploca);
+    while (1)
+    {
+      cout << "Igrač 2, unesi polje figure koju želiš uzeti: ";
+      cin >> unos;
+      transform(unos.begin(), unos.end(), unos.begin(), ::toupper);
+      slova_u_koordinate(unos, pocetna, x, y);
+      if (ploca[x][y] == "●")
+      {
+        ploca[x][y] = pocetna[x][y];
+        clearScreen();
+        brojac1--;
+        break;
+      }
+      else
+        cout << "Na tom polju nije protivnička figura. Pokušaj ponovno." << endl;
+    }
+    return;
+  }
   string unos;
   ispisPloce(ploca);
   while(1){
@@ -133,17 +280,20 @@ void igrac2_uzima(string ploca[9][21], string pocetna[9][21], int &x, int &y){
     transform(unos.begin(), unos.end(), unos.begin(), ::toupper);
     slova_u_koordinate(unos, pocetna, x, y);
     if (ploca[x][y] == "●"){
+      if (figura_je_u_mlinu(x, y, ploca))
+      {
+        cout << "Ta figura je u mlinu. Pokušaj ponovno" << endl;
+        continue;
+      }
       ploca[x][y] = pocetna[x][y];
       clearScreen();
-      brojac2--;
+      brojac1--;
       break;
     }
-    else{
+    else
       cout << "Na tom polju nije protivnička figura. Pokušaj ponovno." << endl;
-    }
   }
 }
-
 void figure_igrac1(string ploca[9][21], string polje, int &x, int &y, string pocetna[9][21])
 {
   slova_u_koordinate(polje, ploca, x, y);
@@ -153,7 +303,6 @@ void figure_igrac1(string ploca[9][21], string polje, int &x, int &y, string poc
   if (provjera_mlina(polje, ploca, emoji))
     igrac1_uzima(ploca, pocetna, x, y);
 }
-
 void figure_igrac2(string ploca[9][21], string polje, int &x, int &y, string pocetna[9][21])
 { 
   slova_u_koordinate(polje, ploca, x, y);
@@ -161,6 +310,156 @@ void figure_igrac2(string ploca[9][21], string polje, int &x, int &y, string poc
   ploca[x][y] = emoji2;
   brojac2++;
   if (provjera_mlina(polje, ploca, emoji2))
+    igrac2_uzima(ploca, pocetna, x, y);
+}
+bool susjedna_polja(const string &a, const string &b)
+{
+  for (int i = 0; i < 24; i++) {
+    if (susjedi[i][0] == a)
+    {
+      for (int j = 1; j<=5 && susjedi[i][j] != ""; j++){
+        if (susjedi[i][j] == b)
+          return true;
+      }
+    }
+  }
+  return false;
+}
+bool ima_slobodno_susjedno_polje(const string &polje, string ploca[9][21], string pocetna[9][21])
+{
+  for (int i = 0; i < 24; i++)
+  {
+    if (susjedi[i][0] == polje)
+    {
+      for (int j = 1; j < 5 && susjedi[i][j] != ""; j++)
+      {
+        string susjed = susjedi[i][j];
+        int x, y;
+        if (slova_u_koordinate(susjed, pocetna, x, y))
+          if (ploca[x][y] == pocetna[x][y])
+            return true;
+      }
+      break;
+    }
+  }
+  return false;
+}
+void pomicanje_igrac1(string ploca[9][21], string pocetna[9][21], int &x, int &y)
+{
+  string odakle, kamo;
+  int x1, y1, x2, y2;
+  while (true)
+  {
+    ispisPloce(ploca);
+    cout << "Igrač 1, unesi polje s kojeg želiš pomaknuti figuru: ";
+    cin >> odakle;
+    transform(odakle.begin(), odakle.end(), odakle.begin(), ::toupper);
+    if (!ima_slobodno_susjedno_polje(odakle, ploca, pocetna))
+    {
+      clearScreen();
+      cout << "Figura na tom polju nema nijedno slobodno susjedno polje za pomicanje." << endl;
+      continue;
+    }
+    if (!slova_u_koordinate(odakle, pocetna, x1, y1))
+    {
+      clearScreen();
+      cout << "To polje ne postoji. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (ploca[x1][y1] != emoji)
+    {
+      clearScreen();
+      cout << "Na tom polju nije tvoja figura. Pokušaj ponovno.\n";
+      continue;
+    }
+    cout << "Unesi polje na koje želiš pomaknuti figuru: ";
+    cin >> kamo;
+    transform(kamo.begin(), kamo.end(), kamo.begin(), ::toupper);
+    if (!slova_u_koordinate(kamo, pocetna, x2, y2))
+    {
+      clearScreen();
+      cout << "To polje ne postoji. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (ploca[x2][y2] != kamo)
+    {
+      clearScreen();
+      cout << "To polje nije prazno. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (!susjedna_polja(odakle, kamo))
+    {
+      clearScreen();
+      cout << "Polja nisu susjedna. Pomicanje nije dozvoljeno.\n";
+      continue;
+    }
+    ploca[x2][y2] = emoji;
+    ploca[x1][y1] = pocetna[x1][y1];
+    x = x2;
+    y = y2;
+    clearScreen();
+    break;
+  }
+  if (provjera_mlina(kamo, ploca, emoji))
+    igrac1_uzima(ploca, pocetna, x, y);
+}
+void pomicanje_igrac2(string ploca[9][21], string pocetna[9][21], int &x, int &y)
+{
+  string odakle, kamo;
+  int x1, y1, x2, y2;
+  while (true)
+  {
+    ispisPloce(ploca);
+    cout << "Igrač 2, unesi polje s kojeg želiš pomaknuti figuru: ";
+    cin >> odakle;
+    transform(odakle.begin(), odakle.end(), odakle.begin(), ::toupper);
+    if (!ima_slobodno_susjedno_polje(odakle, ploca, pocetna))
+    {
+      clearScreen();
+      cout << "Figura na tom polju nema nijedno slobodno susjedno polje za pomicanje." << endl;
+      continue;
+    }
+    if (!slova_u_koordinate(odakle, pocetna, x1, y1))
+    {
+      clearScreen();
+      cout << "To polje ne postoji. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (ploca[x1][y1] != emoji2)
+    {
+      clearScreen();
+      cout << "Na tom polju nije tvoja figura. Pokušaj ponovno.\n";
+      continue;
+    }
+    cout << "Unesi polje na koje želiš pomaknuti figuru: ";
+    cin >> kamo;
+    transform(kamo.begin(), kamo.end(), kamo.begin(), ::toupper);
+    if (!slova_u_koordinate(kamo, pocetna, x2, y2))
+    {
+      clearScreen();
+      cout << "To polje ne postoji. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (ploca[x2][y2] != kamo)
+    {
+      clearScreen();
+      cout << "To polje nije prazno. Pokušaj ponovno.\n";
+      continue;
+    }
+    if (!susjedna_polja(odakle, kamo))
+    {
+      clearScreen();
+      cout << "Polja nisu susjedna. Pomicanje nije dozvoljeno.\n";
+      continue;
+    }
+    ploca[x2][y2] = emoji2;
+    ploca[x1][y1] = pocetna[x1][y1];
+    x = x2;
+    y = y2;
+    clearScreen();
+    break;
+  }
+  if (provjera_mlina(kamo, ploca, emoji2))
     igrac2_uzima(ploca, pocetna, x, y);
 }
 
@@ -252,6 +551,20 @@ int main()
               cout << "Krivi unos\nUnesite ponovno: ";
             }
           } while (dobra_pozicija == false);
+        }
+      }
+      clearScreen();
+      while(brojac1>2 || brojac2>2){
+        int x, y;
+        int i = 0;
+        if (i % 2 == 0)
+        {
+          pomicanje_igrac1(ploca, pocetna, x, y);
+        }
+        i++;
+        if (i % 2 == 1)
+        {
+          pomicanje_igrac2(ploca, pocetna, x, y);
         }
       }
     }
